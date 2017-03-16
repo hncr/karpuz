f340e23fcf42fe0446d960901f70124c73331010
Hasan Cetiner <hsncetiner@gmail.com>
2017-03-16 23:01:15 +0200

    usb and gpu passtrough successful but gta5 fps low

--- titan-qemu.sh
+++ titan-qemu.sh
@@ -1,14 +1,30 @@
-/usr/libexec/qemu-kvm -name titan \
-		-pidfile /tmp/titan.pid \
-		-cpu host -enable-kvm \
-		-smp 4,sockets=1,cores=4,threads=1 \
-		-m 6G \
-		-drive format=raw,file=/mnt/titan.img,if=ide,media=disk \
-		-machine type=pc,accel=kvm \
-		-vga none \
-		-device vfio-pci,host=01:00.0,multifunction=on,romfile=174798.rom\
-		-device vfio-pci,host=01:00.1 \
-       		-vga qxl -spice port=3001,disable-ticketing -soundhw hda \
-		-device virtio-serial -chardev spicevmc,id=vdagent,debug=0,name=vdagent \
-		-device virtserialport,chardev=vdagent,name=com.redhat.spice.0 \
-	        -usbdevice tablet
+PARAMS="$PARAMS -name titan "
+PARAMS="$PARAMS -pidfile /tmp/titan.pid" 
+PARAMS="$PARAMS -cpu host -enable-kvm" 
+PARAMS="$PARAMS -smp 4,sockets=1,cores=4,threads=1" 
+PARAMS="$PARAMS -m 6G" 
+PARAMS="$PARAMS -drive format=raw,file=/mnt/titan.img,if=ide,media=disk" 
+PARAMS="$PARAMS -machine type=pc,accel=kvm" 
+
+#GPU Passtrough
+PARAMS="$PARAMS -device vfio-pci,host=01:00.0,multifunction=on,romfile=174798.rom"
+PARAMS="$PARAMS -device vfio-pci,host=01:00.1" 
+
+#USB Pci-controller pass trough
+#PARAMS="$PARAMS -device vfio-pci,host=00:12.0"
+#PARAMS="$PARAMS -device vfio-pci,host=00:12.2"
+
+#Spice and display for debug
+PARAMS="$PARAMS -vga qxl -spice port=3001,disable-ticketing -soundhw hda" 
+PARAMS="$PARAMS -device virtio-serial -chardev spicevmc,id=vdagent,debug=0,name=vdagent" 
+PARAMS="$PARAMS -device virtserialport,chardev=vdagent,name=com.redhat.spice.0" 
+
+#Spice mouse problem 
+PARAMS="$PARAMS -usbdevice tablet" 
+
+#USB device assign 
+PARAMS="$PARAMS -usb -usbdevice host:17ef:6045 -usbdevice host:046d:c31c"
+
+/usr/libexec/qemu-kvm $PARAMS
+
+
--- titan.sh
+++ titan.sh
@@ -1,10 +1,19 @@
 echo 0000:01:00.0 > /sys/bus/pci/drivers/radeon/unbind
 echo 0000:01:00.1 > /sys/bus/pci/drivers/snd_hda_intel/unbind
+#echo 0000:00:12.0 > /sys/bus/pci/drivers/ohci-pci/unbind
+#echo 0000:00:12.2 > /sys/bus/pci/drivers/ehci-pci/unbind
 modprobe vfio-pci
 echo 1002 6811 > /sys/bus/pci/drivers/vfio-pci/new_id
 echo 1002 aab0 > /sys/bus/pci/drivers/vfio-pci/new_id
-titan-qemu.sh
+#echo 1002 4397 > /sys/bus/pci/drivers/vfio-pci/new_id
+#echo 1002 4396 > /sys/bus/pci/drivers/vfio-pci/new_id
+
+sh titan-qemu.sh
+echo $(date +%H:%M) >> titan.log
+echo $? >> titan.log
 echo 1 > /sys/bus/pci/devices/0000\:01\:00.0/remove
 echo 1 > /sys/bus/pci/devices/0000\:01\:00.1/remove
+#echo 1 > /sys/bus/pci/devices/0000\:00\:12.0/remove
+#echo 1 > /sys/bus/pci/devices/0000\:00\:16.2/remove
 echo 1 > /sys/bus/pci/rescan
 
